bb2b199c3b87be74c550f4de9fd14133767a1b0c
AIN <mdnoor20205@gmail.com>
2023-03-20 13:48:03 +0530

    added super user ccess

--- app/core/models.py
+++ app/core/models.py
@@ -1,3 +1,36 @@
+"""
+Databse models
+"""
 from django.db import models
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    BaseUserManager,
+    PermissionsMixin
+)
 
-# Create your models here.
+class UserManager(BaseUserManager):
+    """Manager for users."""
+    
+    def create_user(self,email,password=None,**extra_fields):
+        if not email:
+            raise ValueError('User must have an Email')
+        user = self.model(email=self.normalize_email(email),**extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+    
+    def create_superuser(self,email,password=None,**extra_fields):
+        """ Create and return a superUser"""
+        user = self.create_user(email,password,**extra_fields),
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+class User(AbstractBaseUser, PermissionsMixin):
+    """ User in the system"""
+    email = models.EmailField(max_length=255, unique=True)
+    name= models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    
+    objects = UserManager() # assigning usermanager class to user 
+    USERNAME_FIELD = 'email' # replace defualt username filed with email
